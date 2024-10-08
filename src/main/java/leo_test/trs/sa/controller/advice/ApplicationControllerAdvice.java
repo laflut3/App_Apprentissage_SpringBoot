9d5c6fe32d37b329f2b1a2463faba1d726fae39a
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-10-08 11:10:42 +0200

    feat(): ajout other exeption

--- src/main/java/leo_test/trs/sa/controller/advice/ApplicationControllerAdvice.java
+++ src/main/java/leo_test/trs/sa/controller/advice/ApplicationControllerAdvice.java
@@ -4,15 +4,24 @@ import jakarta.persistence.EntityNotFoundException;
 import leo_test.trs.sa.dto.ErrorEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class ApplicationControllerAdvice {
+
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler({EntityNotFoundException.class})
-    public ErrorEntity handleException(EntityNotFoundException e) {
+    public @ResponseBody ErrorEntity handleException(EntityNotFoundException e) {
+        return new ErrorEntity(null, e.getMessage());
+    }
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler({RuntimeException.class})
+    public @ResponseBody ErrorEntity handleRuntimeException(RuntimeException e) {
         return new ErrorEntity(null, e.getMessage());
     }
 }
