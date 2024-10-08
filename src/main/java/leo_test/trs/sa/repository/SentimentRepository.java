a924a79807f51dbb621be58492ae8b3fe5f14117
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-10-08 09:55:21 +0200

    feat(): ajout fonction filtrer par type

--- src/main/java/leo_test/trs/sa/repository/SentimentRepository.java
+++ src/main/java/leo_test/trs/sa/repository/SentimentRepository.java
@@ -1,7 +1,11 @@
 package leo_test.trs.sa.repository;
 
 import leo_test.trs.sa.entites.Sentiment;
+import leo_test.trs.sa.enums.TypeSentiment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SentimentRepository  extends JpaRepository<Sentiment, Integer> {
+    List<Sentiment> findByType(TypeSentiment type);
 }
